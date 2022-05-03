5b7e74bda67d3a655fdf7343fd3befee39f03cfb
flanaomi <flavia_ando@hotmail.com>
2022-05-03 12:36:09 +0900

    commit inicial - adicionando os arquivos

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function github(){
+    window.open("https://github.com/flanaomi");
+}
+
+function linkedin(){
+    window.open("https://www.linkedin.com/in/flavianaomiandomizael/");
+
+}
+
+function instagram(){
+    window.open("https://www.instagram.com/flavianaomi_/");
+}
